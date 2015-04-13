804797df5a71c9626b952d1efa8c224bcdebcce4
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-12 23:55:11 -0500

    Finished tutorial.

--- app/src/main/java/com/example/chris/renderscripttutorial/RenderscriptTester.java
+++ app/src/main/java/com/example/chris/renderscripttutorial/RenderscriptTester.java
@@ -2,12 +2,14 @@ package com.example.chris.renderscripttutorial;
 
 import android.content.Context;
 import android.renderscript.Allocation;
+import android.renderscript.Element;
 import android.renderscript.RenderScript;
+import android.renderscript.Script;
+import android.renderscript.Type;
 
-import java.lang.reflect.Type;
 
 /**
- * Created by Chris on 4/11/2015.
+ * Created by Christopher Schneider on 4/11/2015.
  */
 public class RenderscriptTester {
 
@@ -19,8 +21,8 @@ public class RenderscriptTester {
     Allocation allocOut;
 
     //Types
-    Type inputType;
-    Type outputType;
+    Type.Builder inputType;
+    Type.Builder outputType;
 
     //Renderscript object
     RenderScript rs;
@@ -32,6 +34,17 @@ public class RenderscriptTester {
     //Context
     Context context;
 
+    //Allocation USAGE
+    int USAGE = Allocation.USAGE_SCRIPT;
+
+    //Launch options to determine how many iterations over data
+    Script.LaunchOptions launchOptions;
+
+    //Matrix size
+    int matrixWidth = 8;
+    int matrixHeight = 8;
+
+
     RenderscriptTester(Context context){
         this.context = context;
         generateData();
@@ -42,25 +55,75 @@ public class RenderscriptTester {
 
 
     private void generateData(){
-        dataOutput = new byte[50];
-        dataInput = new byte[50];
+        dataOutput = new byte[matrixWidth*matrixHeight];
+        dataInput = new byte[matrixWidth*matrixHeight];
 
-        for(int i = 0; i < 50; i++){
-            dataInput[i] = (byte)(i+1);
+        byte b = 1;
+        for(int i = 0; i < matrixWidth*matrixHeight; i++){
+            dataInput[i] = b++;
         }
-
-    }
-
-    private void initAllocations(){
-
     }
 
     private void buildTypes(){
-        sampleKernel = new ScriptC_SampleKernel(rs);
+        rs = RenderScript.create(context);
+
+        //Input/Output type is U8_4
+        //This is byte in Java, uchar in Renderscript. The 4 represents 4 bytes per element
+        inputType = new Type.Builder(rs, Element.U8_4(rs))
+                .setX(matrixWidth/4)   //These values determine the amount of memory allocated
+                .setY(matrixHeight);   //In this case, we divide by four because the U8_4 has four bytes
+        outputType = new Type.Builder(rs, Element.U8_4(rs))
+                .setX(matrixWidth/4)
+                .setY(matrixHeight);
     }
 
 
+    private void initAllocations(){
+        allocIn = Allocation.createTyped(rs, inputType.create(), USAGE);
+        allocOut = Allocation.createTyped(rs, outputType.create(), USAGE);
+    }
+
+
+
+
     private void runScript(){
+        //Create script instance first
+        sampleKernel = new ScriptC_SampleKernel(rs);
+
+        //Set allocations in script
+        sampleKernel.set_rsAllocationIn(allocIn);
+        sampleKernel.set_rsAllocationOut(allocOut);
+
+        //Copy data into allocation
+        allocIn.copyFrom(dataInput);
+
+        //Invoke script
+        sampleKernel.forEach_root(allocIn, allocOut);
+
+        //Retrieve data from allocation
+        allocOut.copyTo(dataOutput);
+
+        //Results!
+        String input = "INPUT DATA: \n";
+        String output = "OUTPUT DATA: \n";
+
+        for(int i = 0; i < matrixWidth; i++){
+            input += "\n";
+            for(int j = 0; j < matrixHeight; j++){
+                input += String.format("%3d ", dataInput[i*matrixWidth + j]);
+            }
+        }
+        input += "\n";
+
+        for(int i = 0; i < matrixWidth; i++){
+            output += "\n";
+            for(int j = 0; j < matrixHeight; j++){
+                output += String.format("%3d ", dataOutput[i*matrixWidth + j]);
+            }
+        }
+
+        System.out.println(input);
+        System.out.println(output);
 
     }
 
--- app/src/main/rs/SampleKernel.rs
+++ app/src/main/rs/SampleKernel.rs
@@ -5,6 +5,34 @@
 rs_allocation rsAllocationIn;
 rs_allocation rsAllocationOut;
 
-void root(uchar4* in, uint32_t x, uint32_t y) {
-    
+void root(const uchar4* in, uchar4* out, uint32_t x, uint32_t y) {
+    uchar4 modifiedData;
+
+    //Get item from input allocation
+    modifiedData = rsGetElementAt_uchar4(rsAllocationIn, x, y);
+
+    uchar addVal = 10;
+    //Increment all values by addVal
+    modifiedData.w += addVal;
+    modifiedData.x += addVal;
+    modifiedData.y += addVal;
+    modifiedData.z += addVal;
+
+    //Place modified data in output allocation
+    rsSetElementAt_uchar4(rsAllocationOut, modifiedData, x, y);
+
+
+
+    /**=========================================================================================**/
+    /**Another way to modify data. Renderscript automatically populates the in and out data
+        in the function prototypes                                                              **/
+    /**=========================================================================================**/
+
+
+        //This also works; Renderscript automatically populates the uchar4* in
+        //parameter in the function prototype
+        out->w = (in->w)+addVal;
+        out->x = (in->x)+addVal;
+        out->y = (in->y)+addVal;
+        out->z = (in->z)+addVal;
 }
